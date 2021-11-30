0992bc7d20eb64ba0dd5b3aa8ed83377a7a8bc5a
manucarenjot <carenjotje@gmail.com>
2021-11-30 16:33:23 +0100

    Chi fu mi

--- app.js
+++ app.js
@@ -4,12 +4,22 @@ let joueur2 = document.getElementById('joueur2');
 
 let cissor = document.getElementById('cissor');
 let rock = document.getElementById('rock');
-let papper = document.getElementById('rock');
+let papper = document.getElementById('papper');
 
 let tableau = ['cissor', 'rock', 'papper'];
 
 
 cissor.addEventListener("click", function (){
+    let randomTab = Math.round(Math.random() * tableau.length);
+    joueur2.innerHTML = tableau[randomTab];
+})
+
+rock.addEventListener("click", function (){
+    let randomTab = Math.round(Math.random() * tableau.length);
+    joueur2.innerHTML = tableau[randomTab];
+})
+
+papper.addEventListener("click", function (){
     let randomTab = Math.round(Math.random() * tableau.length);
     joueur2.innerHTML = tableau[randomTab];
 })
