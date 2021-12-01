d1fd3e5b1456e12e2b264d4d00f0e9ea9a2c0578
manucarenjot <carenjotje@gmail.com>
2021-12-01 10:23:17 +0100

    chi fu mi

--- app.js
+++ app.js
@@ -6,27 +6,48 @@ let cissor = document.getElementById('cissor');
 let rock = document.getElementById('rock');
 let papper = document.getElementById('papper');
 
-let tableau = ['cissor', 'rock', 'papper'];
+let ciseaux = 'ciseaux';
+let papier = 'papier';
+let pierre = 'pierre';
+
+let player2 = '';
+
+let tableau = [ciseaux, papier, pierre];
 
 
 //à modifier
 function pcGame() {
     let randomTab = Math.round(Math.random() * tableau.length);
 
-    let reponse = randomTab
-        joueur2.innerHTML = tableau[reponse];
+    console.log(randomTab)
+    player2 = randomTab
+    console.log(player2)
+        joueur2.innerHTML = tableau[randomTab];
+
+
+
 
 }
 
 
 cissor.addEventListener("click", function (){
+
     pcGame()
+    if (joueur2.innerHTML === 'pierre'){
+        alert('perdu');
+    }
 })
 
 rock.addEventListener("click", function (){
     pcGame()
+    if (joueur2.innerHTML === 'papier'){
+        alert('perdu');
+    }
 })
 
 papper.addEventListener("click", function (){
     pcGame()
+    if (joueur2.innerHTML === 'ciseaux'){
+        alert('perdu');
+    }
 })
